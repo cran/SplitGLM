34d1584746b3cc1afaf14acd01b29e38f7da341f
Kenbrown3 <78091862+Kenbrown3@users.noreply.github.com>
2022-07-04 15:22:31 -0700

    Update CV_Split_WEN.cpp

--- src/CV_Split_WEN.cpp
+++ src/CV_Split_WEN.cpp
@@ -585,7 +585,7 @@ double CV_Split_WEN::Logistic_Deviance(arma::mat x, arma::vec y,
   //binomial dev
   //return(-2*arma::mean(y % (arma::mean(intercept) + x*arma::mean(betas,1)) - arma::log(1.0 + arma::exp(arma::mean(intercept) + x*arma::mean(betas,1)))));
   //scaled binomial dev
-  return(-2 * arma::mean((x.n_rows/(2*arma::accu(y)) * y % (arma::mean(intercept) + x * arma::mean(betas, 1)) - (n/(2*arma::accu((y-1)*-1)) * arma::log(1.0 + arma::exp(arma::mean(intercept) + x * arma::mean(betas, 1)))));
+  return(-2 * arma::mean((x.n_rows/(2*arma::accu(y))) * y % (arma::mean(intercept) + x * arma::mean(betas, 1)) - (x.n_rows /(2*(arma::accu((y-1)*-1)))) * arma::log(1.0 + arma::exp(arma::mean(intercept) + x * arma::mean(betas, 1)))));
   //exp loss
   //return(-2*arma::mean(y % (arma::mean(intercept) + x*arma::mean(betas,1)) - arma::log(1.0 + arma::exp(arma::mean(intercept) + x*arma::mean(betas,1)))));
   //CE Loss
