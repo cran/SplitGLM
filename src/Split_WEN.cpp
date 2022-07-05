a2e1a4aa4bf8dd9f82b34d68f0ca75fa0c6f6730
Kenbrown3 <78091862+Kenbrown3@users.noreply.github.com>
2022-07-04 19:14:06 -0700

    Update Split_WEN.cpp

--- src/Split_WEN.cpp
+++ src/Split_WEN.cpp
@@ -167,12 +167,12 @@ void Split_WEN::Adjust_Expected_Weights(arma::uword & group){
 
 // Function to adjust the residuals
 void Split_WEN::Adjust_Residuals(arma::uword & group){
-  //residuals.col(group) = y - expected_val.col(group);
-  br.col(group) = y - expected_val.col(group); 
+  residuals.col(group) = y - expected_val.col(group);
+  /*br.col(group) = y - expected_val.col(group); 
   er.col(group) = pow(t*arma::abs(br),o);
   for(std::size_t i=0;i<residuals.col(group).n_elem;i++){
     residuals.col(group)[i] = std::pow(br.col(group)[i],er.col(group)[i]);
-  }
+  }*/
 }
 // Iterative Soft function
 double Split_WEN::Soft(double z, double gamma){
