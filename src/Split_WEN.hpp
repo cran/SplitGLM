84fcef267824031f646375e305138d013cf0664e
Kenbrown3 <78091862+Kenbrown3@users.noreply.github.com>
2022-07-04 18:15:09 -0700

    Update Split_WEN.hpp

--- src/Split_WEN.hpp
+++ src/Split_WEN.hpp
@@ -80,7 +80,7 @@ class Split_WEN{
   
   public:
   
-  void elementwise_pow(const Object& base, const Object& p);
+  Object elementwise_pow(const Object& base, const Object& p);
 
   // Constructor - with data
   Split_WEN(arma::mat x, arma::vec y,
