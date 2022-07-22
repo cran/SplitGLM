cbb138c483f1e251e6d7540943497f33a3027420
Kenbrown3 <78091862+Kenbrown3@users.noreply.github.com>
2022-07-21 23:18:33 -0700

    Update CV_Split_WEN.cpp

--- src/CV_Split_WEN.cpp
+++ src/CV_Split_WEN.cpp
@@ -586,8 +586,8 @@ double CV_Split_WEN::Logistic_Deviance(arma::mat x, arma::vec y,
   //return(-2*arma::mean(y % (arma::mean(intercept) + x*arma::mean(betas,1)) - arma::log(1.0 + arma::exp(arma::mean(intercept) + x*arma::mean(betas,1)))));
   //scaled binomial dev
   //return(-2 * arma::mean((x.n_rows/(2*arma::accu(y))) * y % (arma::mean(intercept) + x * arma::mean(betas, 1)) + (x.n_rows /(2*(arma::accu((y-1)*-1)))) * arma::log(1.0 + arma::exp(arma::mean(intercept) + x * arma::mean(betas, 1)))));
-  //exp loss
-  return(x.n_rows/arma::accu(arma::exp(arma::mean(intercept) + x * arma::mean(betas, 1))));
+  //log loss
+  return((-1*y%arma::log(arma::mean(intercept) + x*arma::mean(betas,1)))+(-1*(1-y)%arma::log(1-arma::mean(intercept) + x*arma::mean(betas,1))));
 }
 // Gamma Deviance (MSPE)
 double CV_Split_WEN::Gamma_Deviance(arma::mat x, arma::vec y,
